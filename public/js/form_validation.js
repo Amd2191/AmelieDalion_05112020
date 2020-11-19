598571afa9abaf7efbd7139b40ec562225046c9a
Amélie Dalion <amelie.dalion@gmail.com>
2020-11-19 18:34:23 +0100

    Post of order and storage of order information returned from backend

--- public/js/form_validation.js
+++ public/js/form_validation.js
@@ -6,10 +6,76 @@ const address = document.getElementById('customer-address');
 const zipcode = document.getElementById('customer-zipcode');
 const city = document.getElementById('customer-city');
 
+function postOrder(data) {
+    fetch("http://localhost:3000/api/teddies/order",{
+    method:'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    mode:'cors',
+    body: data
+    })
+    .then(response=>{
+        console.log(response);
+        return response.json();
+    })
+    .then ( storeOrder => {
+
+        let clientInformation=JSON.stringify(storeOrder.contact);
+        let orderId=JSON.stringify(storeOrder.orderId);
+        console.log(storeOrder.contact);
+        console.log(storeOrder.orderId);
+        let totalOrder=JSON.stringify(totalCart);
+        let orderDetails=[clientInformation,orderId,totalOrder];
+        console.log(orderDetails);
+        localStorage.clear('currentCart');
+        localStorage.clear('orderInformation');
+        localStorage.setItem('orderInformation',orderDetails)
+
+    })
+    
+    
+    
+    // (function storeOrder(){
+        // let clientInformation=JSON.stringify(storeOrder.contact);
+       //  let orderId=JSON.stringify(storeOrder.orderId);
+       //  console.log(storeOrder.contact);
+        // console.log(storeOrder.orderId);
+       //  let totalOrder=JSON.stringify(totalCart);
+        // let orderDetails=[clientInformation,orderId,totalOrder]
+        // console.log(orderDetails);
+        // localStorage.clear('currentCart');
+        // localStorage.setItem('orderInformation',orderDetails)
+   //  })
+    .catch((e) => {
+        console.log(e);
+    })
+}
+
+
+// Submit form with check and sent to the backend
 form.addEventListener('submit', e => {
     e.preventDefault();
-    checkInputs();
+    if (checkInputs()) {
+        let idArray = [];
+        productList.forEach((product) => {
+            idArray.push(product.id);
+        });
+        var order = {
+            contact: {
+                firstName: firstname.value,
+                lastName: lastname.value,
+                address: address.value,
+                city: city.value,
+                email: email.value
+            },
+            products: idArray
+        };
+        console.log(JSON.stringify(order));
+        postOrder(JSON.stringify(order));
+    }
 });
+
 
 function checkInputs() {
     // trim to remove the whitespaces
@@ -18,49 +84,52 @@ function checkInputs() {
     const emailValue = email.value.trim();
     const addressValue = address.value.trim();
     const zipcodeValue = zipcode.value.trim();
+
     // Get the value where whitespaces are allowed
     const cityValue = city.value;
 
+    let validation = true;
+
     // Test of all the imput for invalid imput or empty field
-    if (firstnameValue === '') {
-        setErrorFor(firstname, 'Saisie obligatoire');
-    } else if (!isNotDigit(firstnameValue)) {
+    if (!isNotDigit(firstnameValue)) {
         setErrorFor(firstname, "Prénom erroné");
+        validation = false;
     } else {
         setSuccessFor(firstname);
     }
-    if (lastnameValue === '') {
-        setErrorFor(lastname, 'Saisie obligatoire');
-    } else if (!isNotDigit(lastnameValue)) {
-        setErrorFor(lastname, "Prénom erroné");
+
+    if (!isNotDigit(lastnameValue)) {
+        setErrorFor(lastname, "Nom erroné");
+        validation = false;
     } else {
         setSuccessFor(lastname);
     }
-    if (emailValue === '') {
-        setErrorFor(email, 'Saisie obligatoire');
-    } else if (!isEmail(emailValue)) {
+
+    if (!isEmail(emailValue)) {
         setErrorFor(email, "Votre adresse mail n'est pas valide");
+        validation = false;
     } else {
         setSuccessFor(email);
     }
-    if (addressValue === '') {
-        setErrorFor(address, 'Saisie obligatoire');
-    } else {
-        setSuccessFor(address);
-    }
-    if (cityValue === '') {
-        setErrorFor(city, 'Saisie obligatoire');
-    } else {
-        setSuccessFor(city);
-    }
-    if (zipcodeValue === '') {
-        setErrorFor(zipcode, 'Saisie obligatoire');
-    } else if (!isZipcode(zipcodeValue)) {
+
+    if (!isZipcode(zipcodeValue)) {
         setErrorFor(zipcode, "Code postal erroné");
-    } else{
+        validation = false;
+    } else {
         setSuccessFor(zipcode);
     }
 
+    setSuccessFor(city);
+    setSuccessFor(address);
+
+    document.querySelectorAll('input').forEach(input => {
+        if (!input.value.length) {
+            setErrorFor(input, 'Saisie obligatoire')
+        }
+    })
+    if (validation)
+        return true
+    return false
 }
 
 function setErrorFor(input, message) {
@@ -84,5 +153,5 @@ function isZipcode(zipcode) {
 }
 
 function isNotDigit(input) {
-    return /[\D.]/.test(input);
+    return /^([a-zA-ZàáâäæçéèêëîïôœùûüÿÀÂÄÆÇÉÈÊËÎÏÔŒÙÛÜŸ \-\']+)$/.test(input);
 }
